a1be2e0ed699143f27466e67d44b6cbd54371251
Yokkin <yokkin.h@gmail.com>
2023-06-10 22:51:53 +0900

    name changed

--- src/destroy-meiryo.user.js
+++ src/destroy-meiryo.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name           userscript
+// @name           destroy-meiryo
 // @author         froggie3
 // @description    A custom userstyle that replace some Meiryo or other cringy-looking fonts with BIZ UDPGothic.
 // @downloadURL    https://github.com/froggie3/userscript/raw/main/src/destroy-meiryo.user.js
 // @grant          none
-// @homepage       https://github.com/froggie3/userscript.git
+// @homepage       https://github.com/froggie3/destroy-meiryo.git
 // @match          *://*/*
 // @run-at         document-start
-// @source         https://github.com/froggie3/userscript.git
-// @supportURL     https://github.com/froggie3/userscript.git
+// @source         https://github.com/froggie3/destroy-meiryo.git
+// @supportURL     https://github.com/froggie3/destroy-meiryo.git
 // @updateURL      https://github.com/froggie3/userscript/raw/main/src/destroy-meiryo.user.js
 // @version        1.0.0
 // ==/UserScript==
