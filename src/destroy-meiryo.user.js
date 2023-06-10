e65fed2af00df268ae4ab137f315fa6666cdf6f0
Yokkin <yokkin.h@gmail.com>
2023-06-10 23:45:13 +0900

    aaa

--- src/destroy-meiryo.user.js
+++ src/destroy-meiryo.user.js
@@ -10,45 +10,41 @@
 // @source         https://github.com/froggie3/destroy-meiryo.git
 // @supportURL     https://github.com/froggie3/destroy-meiryo.git
 // @updateURL      https://github.com/froggie3/userscript/raw/main/src/destroy-meiryo.user.js
-// @version        1.0.1
+// @version        1.0.2
 // ==/UserScript==
 
 (() => {
-  'use strict'
-  const toApplymonospace = 'tt, pre, code, kbd, samp, var, textarea'
-  const toApplySansSerif = `div, p, h1, h2, h3, h4, h5, h6, span, input, button :not(${toApplymonospace})`
-  const fontPreferences = '-apple-system, BlinkMacSystemFont, "Segoe UI Adjusted", "Segoe UI", "Liberation Sans", sans-serif'
-  const searchFonts = [
-    /sans-serif/, /[Mm]eiryo/, /Hiragino Kaku/, /Yu\s?Gothic/, /MS PGothic/, /メイリオ/,
-    /ヒラギノ角ゴ/, /游ゴシック/, /ＭＳ Ｐゴシック/
-  ]
+    'use strict'
+    const toApplymonospace = 'tt, pre, code, kbd, samp, var, textarea'
+    const toApplySansSerif = `div, p, h1, h2, h3, h4, h5, h6, span, input, button, ul, ol :not(${toApplymonospace})`
+    const fontPreferences = '-apple-system, BlinkMacSystemFont, "Segoe UI Adjusted", "Segoe UI", "Liberation Sans", sans-serif'
+    const searchFonts = [
+        /sans-serif/, /[Mm]eiryo/, /Hiragino Kaku/, /Yu\s?Gothic/, /MS PGothic/, /メイリオ/,
+        /ヒラギノ角ゴ/, /游ゴシック/, /ＭＳ Ｐゴシック/
+    ]
 
-  const observer = new MutationObserver(() => {
-    for (const i of document.querySelectorAll(toApplySansSerif)) {
-      const testFonts = getComputedStyle(i).fontFamily
-      for (const j of searchFonts) {
-        if (j.test(testFonts)) {
-          i.style.fontFamily = fontPreferences
-          break
+    const observer = new MutationObserver(() => {
+        for (const i of document.querySelectorAll(toApplySansSerif)) {
+            const testFonts = getComputedStyle(i).fontFamily
+            for (const j of searchFonts) {
+                if (j.test(testFonts)) {
+                    i.style.fontFamily = fontPreferences
+                    break
+                }
+            }
         }
-      }
+    })
+    function observeIfElementsReady() {
+        const targetNode = document.querySelector('body')
+        if (targetNode) {
+            observer.observe(targetNode, {
+                attributes: false,
+                childList: true,
+                subtree: true
+            })
+        } else {
+            window.setTimeout(observeIfElementsReady, 500)
+        }
     }
-  })
-  function observeIfElementsReady () {
-    const targetNode = document.querySelector('body')
-    if (targetNode) {
-      observer.observe(targetNode, {
-        attributes: false,
-        childList: true,
-        subtree: true
-      })
-    } else {
-      window.setTimeout(observeIfElementsReady, 500)
-    }
-  }
-  observeIfElementsReady()
-
-  /* Array.from(document.querySelectorAll(toApplymonospace)).map(x => {
-    x.style.fontFamily = "monospace, 'BIZ UDGothic', !important";
-}); */
+    observeIfElementsReady()
 })()
